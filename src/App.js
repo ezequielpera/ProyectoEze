8d5a7b4a99aa89837da7c093ed4ee78f6e540f28
Ezequiel <ezequieldila@gmail.com>
2022-08-20 11:23:10 -0300

    Commit uno

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './components/NavBar';
+
 const App = () => {
 
-  const pagina = {nombre: "Ezequiel", titulo: "Libros"}
 
   return (
     <div className="App">
+      <div className="Container">
+        <div className="TituloUno"><h1>HERMES</h1></div>
+        <div className="TituloDos"><h2>VENTA DE LIBROS</h2></div>
+      </div>
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenidos a {pagina.nombre.toLocaleUpperCase()} {pagina.titulo.toLocaleUpperCase()}.
+          Bienvenidos!
         </p>
         <ButtonComponent/>
         
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <a href="">HOME</a>
+                <a href="">Tienda</a>
+                <a href="">Educativos</a>
+                <a href="">Infantil</a>
+                <a href="">Ficcion</a>
+                <a href="">Esoterismo</a>
+                <a href="">Agendas</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
